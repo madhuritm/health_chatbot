eb28d6ec344b5e1f0720add8db9b8a54d574921d
EC2 Default User <ec2-user@ip-172-31-44-113.ec2.internal>
2025-06-06 19:55:10 +0000

    bug fixed

--- chunking/chunking_code.py
+++ chunking/chunking_code.py
@@ -1,14 +1,36 @@
 import csv
 import nltk
-from nltk.tokenize import sent_tokenize
+import os
+import math
 nltk.download('punkt')
+from nltk.tokenize import sent_tokenize
+
+# Create a directory explicitly if needed
+##os.makedirs(nltk_data_path, exist_ok=True)
+
+# Download punkt to that location
+#nltk.download('punkt', download_dir=nltk_data_path)
+
+# Tell NLTK where to look
+#nltk.data.path.append(nltk_data_path)
+
+nltk_data_path = "/home/ec2-user/nltk_data"
+nltk.data.path.insert(0, nltk_data_path)  # <--- this is the key line
+
+# Just for safety, ensure it's downloaded
+nltk.download('punkt', download_dir=nltk_data_path, quiet=True)
 
 inputCSV="../scraping/scraped_contents.csv"
 
-def getChunks(text: str, chunk_size: int = 100, overlap: float=0.2)->list[str]:
+
+print("NLTK search paths:", nltk.data.path)
+
+def getChunks(text: str, chunk_size: int = 200, overlap: float=0.4)->list[str]:
     sentences=sent_tokenize(text)
     chunks=[]
     current_chunk=[]
+
+    print("Entered getchunks")
 
     i=0
     while i < len(sentences):
@@ -18,6 +40,11 @@ def getChunks(text: str, chunk_size: int = 100, overlap: float=0.2)->list[str]:
         while i < len(sentences) and total_words < chunk_size:
             sentence=sentences[i]
             word_count = len(sentence.split())
+            if word_count > chunk_size and total_words==0:
+                current_chunk.append(sentence)
+                total_words += word_count
+                i += 1
+                break
             if total_words + word_count > chunk_size:
                 break
             current_chunk.append(sentence)
@@ -25,8 +52,10 @@ def getChunks(text: str, chunk_size: int = 100, overlap: float=0.2)->list[str]:
             i+=1
 
         chunks.append(" ".join(current_chunk))
+        if(i==len(sentences)):
+            return chunks
 
-        i=i-int(overlap*len(current_chunk))
+        i=i-round(overlap*len(current_chunk))
     return chunks
 
 def cleanData(inputCSV: str)->list[list[str]]:
@@ -34,7 +63,7 @@ def cleanData(inputCSV: str)->list[list[str]]:
     with open(inputCSV, newline='') as f:
         reader=csv.reader(f)
         for line in reader:
-            if line[0] != 'URL':
+            if line and line[0] != 'URL':
                 chunks=getChunks(line[2])
                 result.append([line[0], line[1], chunks])
             
