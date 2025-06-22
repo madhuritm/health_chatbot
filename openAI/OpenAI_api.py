a9c328a451a33f1c8b2232c781f5ee877afbca99
EC2 Default User <ec2-user@ip-172-31-44-113.ec2.internal>
2025-06-22 04:40:06 +0000

    added comments

--- openAI/OpenAI_api.py
+++ openAI/OpenAI_api.py
@@ -5,17 +5,24 @@ from dotenv import load_dotenv
 import pandas as pd
 import faiss
 
+
+#to load.env
 load_dotenv()
 
 client=OpenAI()
 
+#read the diabetes index that has been created (FAISS index helps to do a very quick semantic search for the query embedding by using optimized DS)
 index=faiss.read_index("../index/diabetes_index.index")
+
+#load the embeddings and stored related metadata 
 df=pd.read_json("../data/chunks_with_embeddings.json", lines=True)
 metadatas=df[['chunk_id', 'chunk_url', 'chunk_title','chunk_text']]
 
+#create embedding from query
 query="How can I prevent high blood sugar?"
 query_embedding=model.encode([query], convert_to_numpy=True).astype('float32')
 
+#get the closest 3 embeddings
 D,I=index.search(query_embedding, k=3)
 
 print(I)
@@ -26,10 +33,11 @@ for idx in I[0]:
     print("-" * 40)
 
 
+#create a chunk of the closest emebddings
 top_k_indices=I[0]
 top_k_chunks = "\n\n".join(metadatas.iloc[idx]['chunk_text'] for idx in top_k_indices)
 
-
+#send query to openAI
 response=client.chat.completions.create(
     model="gpt-3.5-turbo",
     messages=[
@@ -39,8 +47,6 @@ response=client.chat.completions.create(
     ]
 )
 
-
-import re
 
 answer = response.choices[0].message.content
 
