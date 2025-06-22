37936df83594583e235804a8d6ac2fd405e78175
EC2 Default User <ec2-user@ip-172-31-44-113.ec2.internal>
2025-06-22 04:27:41 +0000

    Remove .env from tracking and update .gitignore

---
+++ openAI/OpenAI_api.py
@@ -0,0 +1,52 @@
+from sentence_transformers import SentenceTransformer
+model=SentenceTransformer("sentence-transformers/all-MiniLM-L6-v2")
+from openai import OpenAI
+from dotenv import load_dotenv
+import pandas as pd
+import faiss
+
+load_dotenv()
+
+client=OpenAI()
+
+index=faiss.read_index("../index/diabetes_index.index")
+df=pd.read_json("../data/chunks_with_embeddings.json", lines=True)
+metadatas=df[['chunk_id', 'chunk_url', 'chunk_title','chunk_text']]
+
+query="How can I prevent high blood sugar?"
+query_embedding=model.encode([query], convert_to_numpy=True).astype('float32')
+
+D,I=index.search(query_embedding, k=3)
+
+print(I)
+print(D)
+
+for idx in I[0]:
+    print(metadatas[metadatas['chunk_id'] == int(idx)]['chunk_text'].values[0])
+    print("-" * 40)
+
+
+top_k_indices=I[0]
+top_k_chunks = "\n\n".join(metadatas.iloc[idx]['chunk_text'] for idx in top_k_indices)
+
+
+response=client.chat.completions.create(
+    model="gpt-3.5-turbo",
+    messages=[
+        {"role":"system", "content":"You are a safe and helpful chatbot. Use only the context to answer in not more than 100 words"},
+        {"role":"system", "content":f"Context:\n{top_k_chunks}"},
+        {"role":"user", "content":query}
+    ]
+)
+
+
+import re
+
+answer = response.choices[0].message.content
+
+
+if not answer:
+    answer = "We don't have an answer to that question yet."
+print(f"answer:{answer}")
+
+print("Done!")
