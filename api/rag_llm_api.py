9c55b9ea06e9d11eb5d9f63fbef120a934506a21
EC2 Default User <ec2-user@ip-172-31-44-113.ec2.internal>
2025-07-04 03:09:25 +0000

    cleaned up code

--- api/rag_llm_api.py
+++ api/rag_llm_api.py
@@ -1,87 +1,51 @@
 ##python script to use openai api to get the answer from RAG chunks
 from fastapi import FastAPI
 from pydantic import BaseModel
-import faiss
-from sentence_transformers import SentenceTransformer
-import re
-from dotenv import load_dotenv
-from openai import OpenAI
-import pandas as pd
-import os
 from uuid import uuid4
 from typing import Optional
-
-#load .env file
-load_dotenv()
-
-#set up the openai client
-client=OpenAI()
+from config import client,index,chunks_df,embedder
 
 #set up fast api
 app=FastAPI()
-
-#load the index
-index=faiss.read_index("../index/diabetes_index.index")
-
-#load the chunks
-chunks_df=pd.read_json("../data/chunks_with_embeddings.json", lines=True)
-metadatas=chunks_df[['chunk_id', 'chunk_url', 'chunk_title','chunk_text']]
-
-#load the transformer model
-embedder=SentenceTransformer("sentence-transformers/all-MiniLM-L6-v2")
-
 sessions={}
+
+def extract_k_chunks(query:str)-> tuple[str, list[dict]]:
+    query_embedding=embedder.encode([query]).astype('float32')
+    D,I=index.search(query_embedding, k=3)
+    source_url=[{'url':chunks_df.iloc[idx]['chunk_url']} for idx in I[0]]
+    top_k_chunks="\n\n".join(chunks_df.iloc[idx]['chunk_text'] for idx in I[0])
+    return [top_k_chunks, source_url]
+
+def get_llm_response(history:list[dict], query:str, top_k_chunks:str) -> str:
+    history.append({"role":"user", "content":query})
+    system_prompt={"role":"system", "content":"You are a safe and helpful chatbot. If answer is not in the context, say 'I don't know based on the provided information'. Use current context to answer in not more than 100 words"}
+    context_prompt={"role":"system", "content":f"Context:\n{top_k_chunks}\n\nQuestion:{query}"}
+    messages=[system_prompt, context_prompt] + history
+    response=client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=messages,
+        temperature=0,
+        top_p=1
+    )
+    return response.choices[0].message.content
+
 
 class Question(BaseModel):
     query:str
     session_id:Optional[str]=None
 
 @app.post("/ask")
-def ask(question:Question):
+def ask(question: Question):
     if not question.session_id:
         question.session_id=str(uuid4())
 
-    
-    history=sessions.get(question.session_id, [])
-    history.append({"role":"user", "content":question.query})    
-    system_prompt={"role":"system", "content":"You are a safe and helpful chatbot. If the answer is not in the context, say 'I don’t know based on the provided information'. Use only the context to answer in not more than 100 words"}
-      
+    [top_k_chunks, source_url]=extract_k_chunks(question.query)
 
+    history=sessions.get(question.session_id, [])   
+    answer=get_llm_response(history, question.query, top_k_chunks)
+    history.append({"role":"assistant", "content":answer})    
+    sessions[question.session_id]=history
 
-    ##embed the query
-    query_embedding=embedder.encode([question.query]).astype('float32')
-    ##find the indices closest in the metadata
-    D,I=index.search(query_embedding, k=3)
-
-    source_url=[
-        {
-            'url' : metadatas.iloc[idx]['chunk_url']
-        }
-        for idx in I[0]
-    ]
-
-    ##extract these chunks and make a str
-    top_k_chunks="\n\n".join(chunks_df.iloc[idx]['chunk_text'] for idx in I[0])   
-
-    context_prompt={"role":"system", "content":f"Context:\n{top_k_chunks}\n\nQuestion: {question.query}"}
-
-
-    ##write the openai api
-    response=client.chat.completions.create(
-        model="gpt-3.5-turbo",
-        messages=[system_prompt, context_prompt] + history,
-        temperature=0,
-        top_p=1
-    )
-
-    
-    answer=response.choices[0].message.content
-
-    history.append({"role":"assistant", "content":answer})
-
-    
-    print("Done")
-    
-    return{"answer":answer, "sources":source_url, "chunks":top_k_chunks}
+    return{"answer":answer, "sources":source_url}
 
     
