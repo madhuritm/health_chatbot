97ced2fd59d6f0c02db518bd7356dc9bf636c278
madhuritm <madhurimadhusudan91@gmail.com>
2025-12-09 17:01:51 +0000

    Graph RAG

--- src/config/config.py
+++ src/config/config.py
@@ -6,6 +6,6 @@ import pandas as pd
 
 load_dotenv()
 client=OpenAI()
-index=faiss.read_index("/home/ec2-user/health_chatbot/index/diabetes_index.index")
-chunks_df=pd.read_json("/home/ec2-user/health_chatbot/data/chunks_with_embeddings.json", lines=True)
+index=faiss.read_index("/home/ec2-user/health_chatbot/index/test_asthma_index.index")
+chunks_df=pd.read_json("/home/ec2-user/health_chatbot/embeddings/outputFiles/test_asthma_embeddings.json", lines=True)
 embedder=SentenceTransformer("sentence-transformers/all-MiniLM-L6-v2")
--- src/inference/answerGenerator.py
+++ src/inference/answerGenerator.py
@@ -4,6 +4,14 @@ import os, argparse, sys
 from sklearn.metrics.pairwise import cosine_similarity
 from src.config.config import client, index, chunks_df, embedder
 
+# Make Python aware of the Graphweaver project
+GRAPHWEAVER_ROOT = "/home/ec2-user/Graphweaver"
+if GRAPHWEAVER_ROOT not in sys.path:
+    sys.path.append(GRAPHWEAVER_ROOT)
+
+from graphRetriever.Text2CypherQuery import text2cypher
+
+
 
 api_key = os.getenv("OPENAI_API_KEY")
 
@@ -34,14 +42,27 @@ def cal_cosine_similarity(top_k_chunks:str, answer:str)->float:
     sims=cosine_similarity([chunk_embedding], [answer_embedding])
     return sims
 
-def extract_answer(query: str) -> list[str, str]:
+def extract_answer_with_rag(query: str) -> list[str, str]:
     [top_k_chunks,source_url] = extract_k_chunks(query)
     answer=get_llm_response(query, top_k_chunks)
     sims=cal_cosine_similarity(top_k_chunks, answer)
     print(f"Similarity is {sims[0][0]}")
     return [answer, source_url]
 
+def get_answer_with_graph_and_rag(query: str):
+    #1) Ty graph retrieval first
+    graph_answer, cypher = text2cypher(query)
+
+    if graph_answer:
+        print("using graph answer path")
+        #use graph answer as context to llm
+        graph_context = f"Answer derieved from a medical knowledge graph: {graph_answer}"
+        final_answer = get_llm_response(query, graph_context)
+        return final_answer, []
     
+    print("Falling back to RAG path")
+    # 2) Fall back to RAG if graph has no answer
+    return extract_answer_with_rag(query)
 
 
 def main():
@@ -49,12 +70,18 @@ def main():
     ap.add_argument("--question", required=True)
     args = ap.parse_args()
     query = args.question
-    answer, source_url = extract_answer(query)
-    if answer == "I don’t know based on the provided information.":    
-        output=answer
+
+    answer, source_url = get_answer_with_graph_and_rag(query)
+     # If LLM says it does not know, just return that
+    if answer == "I don’t know based on the provided information.":
+        output = answer
     else:
-        answer=answer + "\n" + "sources"
-        output=answer+"\n"+"\n".join(idx["url"] for idx in source_url)
+        if source_url:
+            answer = answer + "\n" + "sources"
+            output = answer + "\n" + "\n".join(idx["url"] for idx in source_url)
+        else:
+            # Graph path: no sources list
+            output = answer
 
     print(f"answer:{output}")
     print("Done!")
